67809c09d28e13ccc86ab53ea2b81b9ad64064e3
Jesus Magana <maganazero5@gmail.com>
2014-07-04 15:56:14 -0700

    Took out the map generation class and wil make a new one

--- ProceduralLevelGeneration/ProceduralLevelGeneration/Map.h
+++ ProceduralLevelGeneration/ProceduralLevelGeneration/Map.h
@@ -2,15 +2,19 @@
 //  Map.h
 //  ProceduralLevelGeneration
 //
-//  Created by Kim Pedersen on 13/08/13.
-//  Copyright (c) 2013 Kim Pedersen. All rights reserved.
+//  Created by Jesus Magana on 7/4/14.
+//  Copyright (c) 2014 Kim Pedersen. All rights reserved.
 //
 
 #import <SpriteKit/SpriteKit.h>
 
 @interface Map : SKNode
 
+@property (nonatomic) CGSize gridSize;
 @property (nonatomic, readonly) CGPoint spawnPoint;
 @property (nonatomic, readonly) CGPoint exitPoint;
+
++ (instancetype) mapWithGridSize:(CGSize)gridSize;
+- (instancetype) initWithGridSize:(CGSize)gridSize;
 
 @end
